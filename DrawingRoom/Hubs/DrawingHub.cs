9be56d57e79b3f536abb7aaf033e8e58f6a23e58
Hakim Ghods <ghods.hakim@hotmail.com>
2018-12-24 01:32:42 +0330

    Line Draw

--- DrawingRoom/Hubs/DrawingHub.cs
+++ DrawingRoom/Hubs/DrawingHub.cs
@@ -9,5 +9,10 @@ namespace DrawingRoom.Hubs
         {
             await Clients.All.SendAsync("ReceiveCoord", x, y);
         }
+
+        public async Task SendLine(int x1, int y1,int x2,int y2)
+        {
+            await Clients.All.SendAsync("ReceiveLine", x1, y1,x2,y2);
+        }
     }
 }
